35e7c136e63a8bf7b566bbc4d141de29eefc9b1d
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-03-23 15:05:25 -0300

    Transactions and JPA session
    Modulo 1 - CRUD.

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-		
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
+		
 		return repository.findAll();
 	}
 }
